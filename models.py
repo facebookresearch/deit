40db5587668ffc418ad9df90e1c4c55a380cacda
Zhiyuan Chen <chenzhiyuan1@tetras.ai>
2021-01-08 13:40:09 +0800

    add l16

--- models.py
+++ models.py
@@ -55,3 +55,15 @@ def deit_base_patch16_224(pretrained=False, **kwargs):
         )
         model.load_state_dict(checkpoint["model"])
     return model
+
+
+@register_model
+def deit_large_patch16_224(pretrained=False, **kwargs):
+    model = VisionTransformer(
+        patch_size=16, embed_dim=1024, depth=24, num_heads=16, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6))
+    model.default_cfg = _cfg()
+    if pretrained:
+        print('pretrained not available for this model')
+    return model
+
