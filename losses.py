3fc505a77e870ad04edf4d4b06c5a57ba973c27a
Francisco Massa <fvsmassa@gmail.com>
2021-01-13 02:45:12 -0800

    Add docstring for DistillationLoss

--- losses.py
+++ losses.py
@@ -3,15 +3,29 @@ from torch.nn import functional as F
 
 
 class DistillationLoss(torch.nn.Module):
-    def __init__(self, base_criterion, teacher_model, distillation_type, alpha, tau):
+    """
+    This module wraps a standard criterion and adds an extra knowledge distillation loss by
+    taking a teacher model prediction and using it as additional supervision.
+    """
+    def __init__(self, base_criterion: torch.nn.Module, teacher_model: torch.nn.Module,
+                 distillation_type: str, alpha: float, tau: float):
         super().__init__()
         self.base_criterion = base_criterion
         self.teacher_model = teacher_model
+        assert distillation_type in ['none', 'soft', 'hard']
         self.distillation_type = distillation_type
         self.alpha = alpha
         self.tau = tau
 
     def forward(self, inputs, outputs, labels):
+        """
+        Args:
+            inputs: The original inputs that are feed to the teacher model
+            outputs: the outputs of the model to be trained. It is expected to be
+                either a Tensor, or a Tuple[Tensor, Tensor], with the original output
+                in the first position and the distillation predictions as the second output
+            labels: the labels for the base criterion
+        """
         outputs_kd = None
         if not isinstance(outputs, torch.Tensor):
             # assume that the model outputs a tuple of [outputs, outputs_kd]
