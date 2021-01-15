137d5dedfbde79ea669bf4f39082a80e7049fe76
Francisco Massa <fvsmassa@gmail.com>
2021-01-15 13:16:28 -0800

    Fix no-weight-decay in optimizer

--- main.py
+++ main.py
@@ -303,7 +303,7 @@ def main(args):
 
     linear_scaled_lr = args.lr * args.batch_size * utils.get_world_size() / 512.0
     args.lr = linear_scaled_lr
-    optimizer = create_optimizer(args, model)
+    optimizer = create_optimizer(args, model_without_ddp)
     loss_scaler = NativeScaler()
 
     lr_scheduler, _ = create_scheduler(args, optimizer)
