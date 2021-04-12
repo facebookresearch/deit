d3ce3cd6d436aa8dc4640dc4f763a608e3400c04
Yeongjae Jang <54150647+Liberatedwinner@users.noreply.github.com>
2021-04-12 16:02:22 +0900

    Update cait_models.py
    minor update...

--- cait_models.py
+++ cait_models.py
@@ -12,7 +12,7 @@ from timm.models.layers import trunc_normal_
 
 __all__ = [
     'cait_M48', 'cait_M36', 'cait_M4',
-    'cait_S36', 'cait_S24','cait_S24_224',
+    'cait_S36', 'cait_S24', 'cait_S24_224',
     'cait_XS24'
 ]
 
@@ -58,7 +58,7 @@ class LayerScale_Block_CA(nn.Module):
     # with slight modifications to add CA and LayerScale
     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
                  drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, Attention_block = Class_Attention,
-                 Mlp_block=Mlp,init_values=1e-4):
+                 Mlp_block=Mlp, init_values=1e-4):
         super().__init__()
         self.norm1 = norm_layer(dim)
         self.attn = Attention_block(
@@ -67,17 +67,13 @@ class LayerScale_Block_CA(nn.Module):
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp_block(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
-        self.gamma_1 = nn.Parameter(init_values * torch.ones((dim)),requires_grad=True)
-        self.gamma_2 = nn.Parameter(init_values * torch.ones((dim)),requires_grad=True)
+        self.gamma_1 = nn.Parameter(init_values * torch.ones((dim)), requires_grad=True)
+        self.gamma_2 = nn.Parameter(init_values * torch.ones((dim)), requires_grad=True)
 
     
     def forward(self, x, x_cls):
-        
-        u = torch.cat((x_cls,x),dim=1)
-        
-        
+        u = torch.cat((x_cls, x), dim=1)
         x_cls = x_cls + self.drop_path(self.gamma_1 * self.attn(self.norm1(u)))
-        
         x_cls = x_cls + self.drop_path(self.gamma_2 * self.mlp(self.norm2(x_cls)))
         
         return x_cls 
@@ -88,21 +84,14 @@ class Attention_talking_head(nn.Module):
     # with slight modifications to add Talking Heads Attention (https://arxiv.org/pdf/2003.02436v1.pdf)
     def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
         super().__init__()
-        
         self.num_heads = num_heads
-        
         head_dim = dim // num_heads
-        
         self.scale = qk_scale or head_dim ** -0.5
-
         self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
         self.attn_drop = nn.Dropout(attn_drop)
-        
-        self.proj = nn.Linear(dim, dim)
-        
+        self.proj = nn.Linear(dim, dim) 
         self.proj_l = nn.Linear(num_heads, num_heads)
-        self.proj_w = nn.Linear(num_heads, num_heads)
-        
+        self.proj_w = nn.Linear(num_heads, num_heads)    
         self.proj_drop = nn.Dropout(proj_drop)
 
 
@@ -110,17 +99,12 @@ class Attention_talking_head(nn.Module):
     def forward(self, x):
         B, N, C = x.shape
         qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
-        q, k, v = qkv[0] * self.scale , qkv[1], qkv[2] 
-    
+        q, k, v = qkv[0] * self.scale, qkv[1], qkv[2] 
         attn = (q @ k.transpose(-2, -1)) 
-        
-        attn = self.proj_l(attn.permute(0,2,3,1)).permute(0,3,1,2)
-                
+        attn = self.proj_l(attn.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
         attn = attn.softmax(dim=-1)
-  
-        attn = self.proj_w(attn.permute(0,2,3,1)).permute(0,3,1,2)
+        attn = self.proj_w(attn.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
         attn = self.attn_drop(attn)
-
         x = (attn @ v).transpose(1, 2).reshape(B, N, C)
         x = self.proj(x)
         x = self.proj_drop(x)
@@ -130,18 +114,19 @@ class LayerScale_Block(nn.Module):
     # taken from https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
     # with slight modifications to add layerScale
     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
-                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm,Attention_block = Attention_talking_head,
-                 Mlp_block=Mlp,init_values=1e-4):
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, Attention_block = Attention_talking_head,
+                 Mlp_block=Mlp, init_values=1e-4):
         super().__init__()
         self.norm1 = norm_layer(dim)
         self.attn = Attention_block(
-            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
+            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop
+        )
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp_block(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
-        self.gamma_1 = nn.Parameter(init_values * torch.ones((dim)),requires_grad=True)
-        self.gamma_2 = nn.Parameter(init_values * torch.ones((dim)),requires_grad=True)
+        self.gamma_1 = nn.Parameter(init_values * torch.ones((dim)), requires_grad=True)
+        self.gamma_2 = nn.Parameter(init_values * torch.ones((dim)), requires_grad=True)
 
     def forward(self, x):        
         x = x + self.drop_path(self.gamma_1 * self.attn(self.norm1(x)))
@@ -157,22 +142,20 @@ class cait_models(nn.Module):
     def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=768, depth=12,
                  num_heads=12, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0.,
                  drop_path_rate=0., norm_layer=nn.LayerNorm, global_pool=None,
-                 block_layers = LayerScale_Block,
-                 block_layers_token = LayerScale_Block_CA,
-                 Patch_layer=PatchEmbed,act_layer=nn.GELU,
-                 Attention_block = Attention_talking_head,Mlp_block=Mlp,
-                init_scale=1e-4,
-                Attention_block_token_only=Class_Attention,
-                Mlp_block_token_only= Mlp, 
-                depth_token_only=2,
-                mlp_ratio_clstk = 4.0):
+                 block_layers=LayerScale_Block,
+                 block_layers_token=LayerScale_Block_CA,
+                 Patch_layer=PatchEmbed, 
+                 act_layer=nn.GELU,
+                 Attention_block = Attention_talking_head,
+                 Mlp_block=Mlp,
+                 init_scale=1e-4,
+                 Attention_block_token_only=Class_Attention,
+                 Mlp_block_token_only=Mlp, 
+                 depth_token_only=2,
+                 mlp_ratio_clstk = 4.0):
         super().__init__()
-        
-
-            
         self.num_classes = num_classes
         self.num_features = self.embed_dim = embed_dim  
-
         self.patch_embed = Patch_layer(
                 img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
         
@@ -187,21 +170,23 @@ class cait_models(nn.Module):
             block_layers(
                 dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
                 drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer,
-                act_layer=act_layer,Attention_block=Attention_block,Mlp_block=Mlp_block,init_values=init_scale)
-            for i in range(depth)])
+                act_layer=act_layer, Attention_block=Attention_block, Mlp_block=Mlp_block, init_values=init_scale
+            )
+            for i in range(depth)
+        ])
         
 
         self.blocks_token_only = nn.ModuleList([
             block_layers_token(
                 dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio_clstk, qkv_bias=qkv_bias, qk_scale=qk_scale,
                 drop=0.0, attn_drop=0.0, drop_path=0.0, norm_layer=norm_layer,
-                act_layer=act_layer,Attention_block=Attention_block_token_only,
-                Mlp_block=Mlp_block_token_only,init_values=init_scale)
-            for i in range(depth_token_only)])
+                act_layer=act_layer, Attention_block=Attention_block_token_only,
+                Mlp_block=Mlp_block_token_only, init_values=init_scale
+            )
+            for i in range(depth_token_only)
+        ])
             
         self.norm = norm_layer(embed_dim)
-
-
         self.feature_info = [dict(num_chs=embed_dim, reduction=0, module='head')]
         self.head = nn.Linear(embed_dim, num_classes) if num_classes > 0 else nn.Identity()
 
@@ -232,11 +217,11 @@ class cait_models(nn.Module):
         x = x + self.pos_embed
         x = self.pos_drop(x)
 
-        for i , blk in enumerate(self.blocks):
+        for i, blk in enumerate(self.blocks):
             x = blk(x)
             
-        for i , blk in enumerate(self.blocks_token_only):
-            cls_tokens = blk(x,cls_tokens)
+        for i, blk in enumerate(self.blocks_token_only):
+            cls_tokens = blk(x, cls_tokens)
 
         x = torch.cat((cls_tokens, x), dim=1)
             
@@ -259,7 +244,7 @@ def cait_XS24(pretrained=False, **kwargs):
         img_size= 384,patch_size=16, embed_dim=288, depth=24, num_heads=6, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6),
         init_scale=1e-5,
-        depth_token_only=2,**kwargs)
+        depth_token_only=2, **kwargs)
     
     model.default_cfg = _cfg()
     if pretrained:
@@ -269,7 +254,7 @@ def cait_XS24(pretrained=False, **kwargs):
         )
         checkpoint_no_module = {}
         for k in model.state_dict().keys():
-            checkpoint_no_module[k] = checkpoint["model"]['module.'+k]
+            checkpoint_no_module[k] = checkpoint["model"]['module.' + k]
             
         model.load_state_dict(checkpoint_no_module)
         
@@ -284,7 +269,7 @@ def cait_S24_224(pretrained=False, **kwargs):
         img_size= 224,patch_size=16, embed_dim=384, depth=24, num_heads=8, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6),
         init_scale=1e-5,
-        depth_token_only=2,**kwargs)
+        depth_token_only=2, **kwargs)
     
     model.default_cfg = _cfg()
     if pretrained:
@@ -294,7 +279,7 @@ def cait_S24_224(pretrained=False, **kwargs):
         )
         checkpoint_no_module = {}
         for k in model.state_dict().keys():
-            checkpoint_no_module[k] = checkpoint["model"]['module.'+k]
+            checkpoint_no_module[k] = checkpoint["model"]['module.' + k]
             
         model.load_state_dict(checkpoint_no_module)
         
@@ -306,7 +291,7 @@ def cait_S24(pretrained=False, **kwargs):
         img_size= 384,patch_size=16, embed_dim=384, depth=24, num_heads=8, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6),
         init_scale=1e-5,
-        depth_token_only=2,**kwargs)
+        depth_token_only=2, **kwargs)
     
     model.default_cfg = _cfg()
     if pretrained:
@@ -316,7 +301,7 @@ def cait_S24(pretrained=False, **kwargs):
         )
         checkpoint_no_module = {}
         for k in model.state_dict().keys():
-            checkpoint_no_module[k] = checkpoint["model"]['module.'+k]
+            checkpoint_no_module[k] = checkpoint["model"]['module.' + k]
             
         model.load_state_dict(checkpoint_no_module)
         
@@ -328,7 +313,7 @@ def cait_S36(pretrained=False, **kwargs):
         img_size= 384,patch_size=16, embed_dim=384, depth=36, num_heads=8, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6),
         init_scale=1e-6,
-        depth_token_only=2,**kwargs)
+        depth_token_only=2, **kwargs)
     
     model.default_cfg = _cfg()
     if pretrained:
@@ -338,7 +323,7 @@ def cait_S36(pretrained=False, **kwargs):
         )
         checkpoint_no_module = {}
         for k in model.state_dict().keys():
-            checkpoint_no_module[k] = checkpoint["model"]['module.'+k]
+            checkpoint_no_module[k] = checkpoint["model"]['module.' + k]
             
         model.load_state_dict(checkpoint_no_module)
 
@@ -354,7 +339,7 @@ def cait_M36(pretrained=False, **kwargs):
         img_size= 384, patch_size=16, embed_dim=768, depth=36, num_heads=16, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6),
         init_scale=1e-6,
-        depth_token_only=2,**kwargs)
+        depth_token_only=2, **kwargs)
     
     model.default_cfg = _cfg()
     if pretrained:
@@ -364,7 +349,7 @@ def cait_M36(pretrained=False, **kwargs):
         )
         checkpoint_no_module = {}
         for k in model.state_dict().keys():
-            checkpoint_no_module[k] = checkpoint["model"]['module.'+k]
+            checkpoint_no_module[k] = checkpoint["model"]['module.' + k]
             
         model.load_state_dict(checkpoint_no_module)
 
@@ -377,7 +362,7 @@ def cait_M48(pretrained=False, **kwargs):
         img_size= 448 , patch_size=16, embed_dim=768, depth=48, num_heads=16, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6),
         init_scale=1e-6,
-        depth_token_only=2,**kwargs)
+        depth_token_only=2, **kwargs)
     
     model.default_cfg = _cfg()
     if pretrained:
@@ -387,7 +372,7 @@ def cait_M48(pretrained=False, **kwargs):
         )
         checkpoint_no_module = {}
         for k in model.state_dict().keys():
-            checkpoint_no_module[k] = checkpoint["model"]['module.'+k]
+            checkpoint_no_module[k] = checkpoint["model"]['module.' + k]
             
         model.load_state_dict(checkpoint_no_module)
         
