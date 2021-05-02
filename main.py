2f76ec64fdd1626f80e31c7cfae71521aebb2fa6
Maxwell Aladago <maxwell.aladago@gmail.com>
2021-05-02 10:27:25 -0400

    BUG Fix: ensure the right loss function is selected when mixtup or cutmix is enabeled

--- main.py
+++ main.py
@@ -310,7 +310,7 @@ def main(args):
 
     criterion = LabelSmoothingCrossEntropy()
 
-    if mixup_active > 0.:
+    if mixup_active:
         # smoothing is handled with mixup label transform
         criterion = SoftTargetCrossEntropy()
     elif args.smoothing:
