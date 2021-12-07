06e80acefe1bbce63047553562df19266859eb4c
Hugo Touvron <49280617+TouvronHugo@users.noreply.github.com>
2021-12-07 09:15:17 +0100

    add ResMLP and CaiT models

--- hubconf.py
+++ hubconf.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2015-present, Facebook, Inc.
 # All rights reserved.
 from models import *
+from cait_models import *
+from resmlp_models import *
 
 dependencies = ["torch", "torchvision", "timm"]
