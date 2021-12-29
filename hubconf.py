94dab749c231be6ea8fd9b5508fff4dff6d7fc45
Hugo Touvron <49280617+TouvronHugo@users.noreply.github.com>
2021-12-29 16:30:55 +0100

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -3,6 +3,6 @@
 from models import *
 from cait_models import *
 from resmlp_models import *
-from patchconvnet_models import *
+#from patchconvnet_models import *
 
 dependencies = ["torch", "torchvision", "timm"]
